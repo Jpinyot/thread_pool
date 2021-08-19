56adf0964315a144a9dfb62f15e740f8ee7e5dc0
jpinyot <pep.pinyot@gmail.com>
2021-08-19 15:58:36 +0200

    Change raw pointers with values of Task elements

--- include/thread_pool/queue.h
+++ include/thread_pool/queue.h
@@ -1,40 +1,68 @@
 #include <atomic>
+#include <future>
 #include <tuple>
 
 //TODO(jpinyot): change to correct cache line size
 constexpr uint32_t kCacheLineSize = 128;
 
-template<typename T, typename ... Args>
+template<typename F, typename ... Args>
 class Queue {
-    Queue():
-        _first(nullptr),
-        _divider(nullptr),
-        _last(nullptr) {
-        // init list with dummy element
-        _first = _divider = _last = new Task(T());
-    }
+    public:
+        Queue():
+            _first(nullptr), _divider(nullptr), _last(nullptr),
+            _consumerLock(false), _producerLock(false) {
+            // init list with dummy element
+            _first = _divider = _last = new Task(nullptr, nullptr);
+        }
+        ~Queue() {
+            // delete all pending tasks
+            while (_first != nullptr) {
+                Task* tmp = _first;
+                _first = _first->next;
+                delete tmp;
+            }
+        }
+
+    public:
+        void Produce(const F&& function, const Args&& ... args) {
+            Task* tmp = new Task(function, args);
+        }
+
+        std::future<F> Consume() {
+        }
 
     private:
         // class Task is a node of the Queue holding the task to be executed
         class Task {
             public:
                 // TODO(jpinyot): check if makes sense to change pointer to references
-                Task(T* value, Args... args):
-                    _value(value),
+                Task(F&& function, Args&& ... args):
+                    _function(std::forward<F>(function)),
                     _args(std::forward<Args>(args)...),
                     _next(nullptr) {
-                    }
+                }
+                ~Task() = default;
 
             private:
-                T* _value;
-                std::tuple<Args...> _args;
-                std::atomic<T*> _next;
+                // TODO(jpinyot): pass to raw pointers or unique_ptr to avoid blocking
+                // while copyng the argument
+                F _function;
+                std::tuple<Args ...> _args;
+                std::atomic<F> _next;
                 // padding is to ensure that two Tasks objects won't occupy the same chache line
-                char pad[kCacheLineSize - sizeof(T*) - sizeof(std::tuple<Args...>) -
-                    sizeof(std::atomic<Task*>)];
+                /* char pad[kCacheLineSize - sizeof(F*) - sizeof(std::tuple<Args...>) - */
+                /*     sizeof(std::atomic<Task*>)]; */
         };
 
+        //TODO(jpinyot): add padd to each pointer
+        // first element
         Task* _first;
-        std::atomic<Task*> _divider;
-        std::atomic<Task*> _last;
+        // dummy element
+        Task* _divider;
+        // last element
+        Task* _last;
+        // shared among consumers
+        std::atomic<bool> _consumerLock;
+        // shared among producers
+        std::atomic<bool> _producerLock;
 }
