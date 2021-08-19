6590f05b823afe7003819aaa291b827b3fcb3898
jpinyot <pep.pinyot@gmail.com>
2021-08-19 16:38:14 +0200

    Add declaration of Produce() and Consume() in Queue

--- include/thread_pool/queue.h
+++ include/thread_pool/queue.h
@@ -1,5 +1,6 @@
 #include <atomic>
 #include <future>
+#include <functional>
 #include <tuple>
 
 //TODO(jpinyot): change to correct cache line size
@@ -24,12 +25,48 @@ class Queue {
         }
 
     public:
+        // add new task to the queue
         void Produce(const F&& function, const Args&& ... args) {
-            Task* tmp = new Task(function, args);
+            Task* tmp = new Task(std::forward<F>(function), std::forward<Args>(args)...);
+            // wait until acquire exclusivity
+            while (_producerLock.exchange(true)) {
+            }
+
+            // publish to consumers
+            _last->next = tmp;
+            // swing last forward
+            _last = tmp;
+            // release exclusivity
+            _producerLock = false;
         }
 
-        std::future<F> Consume() {
+        // consume existing task
+        // TODO(jpinyot): return the return value of the function
+        bool Consume(F& result) {
+            // wait until acquire exclusivity
+            while (_consumerLock.exchange(true)) {
+            }
+
+            Task* next = _first->next;
+            // if queue is nonempty
+            if (next != nullptr) {
+                // copy function and arguments from the Task
+                F function = next->_function;
+                std::tuple<Args ...> args = next->_args;
+                _first = next;
+                // release exclusivity
+                _consumerLock.exchange(false);
+                //TODO(jpinyot): execute function if exist
+                return true;
+            }
+            // queue is empty
+            else {
+                // release exclusivity
+                _consumerLock.exchange(false);
+                return false;
+            }
         }
+
 
     private:
         // class Task is a node of the Queue holding the task to be executed
@@ -46,12 +83,10 @@ class Queue {
             private:
                 // TODO(jpinyot): pass to raw pointers or unique_ptr to avoid blocking
                 // while copyng the argument
-                F _function;
+                std::function<F> _function;
                 std::tuple<Args ...> _args;
                 std::atomic<F> _next;
-                // padding is to ensure that two Tasks objects won't occupy the same chache line
-                /* char pad[kCacheLineSize - sizeof(F*) - sizeof(std::tuple<Args...>) - */
-                /*     sizeof(std::atomic<Task*>)]; */
+                //TODO(jpinyot): add padd to each pointer
         };
 
         //TODO(jpinyot): add padd to each pointer
@@ -65,4 +100,4 @@ class Queue {
         std::atomic<bool> _consumerLock;
         // shared among producers
         std::atomic<bool> _producerLock;
-}
+};
