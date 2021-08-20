515af71cc1191d21aca1f058d218709fabb360f9
jpinyot <pep.pinyot@gmail.com>
2021-08-20 12:56:47 +0200

    Add wip comment

--- include/thread_pool/queue.h
+++ include/thread_pool/queue.h
@@ -7,6 +7,11 @@
 //TODO(jpinyot): change to correct cache line size
 constexpr uint32_t kCacheLineSize = 128;
 
+// Lock-free Queue of Tasks:
+//  - the first element is ALWAYS a dummy element that is deleted after executing the next element,
+//    after deleting the first element, the executed elements remains as the dummy element.
+//  - Producers threads adds new Tasks to the TAIL of the Queue.
+//  - Consumer threads execute Task on the HEAD of the Queue.
 class Queue {
     public:
         Queue():
