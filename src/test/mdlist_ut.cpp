d01a66b5988659a3e376f3285aa452d89da5763f
jpinyot <pep.pinyot@gmail.com>
2021-09-02 15:41:42 +0200

    Add multi consumer and multi producers threads test in MDListUt

--- src/test/mdlist_ut.cpp
+++ src/test/mdlist_ut.cpp
@@ -7,48 +7,92 @@
 
 using thread_pool::MDList;
 
+constexpr uint32_t kPriorityCount = 3;
+
 class MDListUt : public ::testing::Test{
     public:
         MDListUt():
-            list(nullptr) {
+            _list(nullptr) {
 
         }
 
         void SetUp() {
             // code here will execute just before the test ensues
-            list = new MDList(3);
+            _list = new MDList(kPriorityCount);
         }
 
         void TearDown() {
             // code here will be called just after the test completes
             // ok to through exceptions from here if need be
-            if (list) {
-                delete list;
+            if (_list) {
+                delete _list;
             }
         }
 
         ~MDListUt() {}
 
     protected:
-        MDList* list;
+        MDList* _list;
 };
-
-// bad test
-TEST_F(MDListUt, setupTearDown)
-{
-    list = new MDList(1);
-
-    EXPECT_EQ(1,1);
-}
 
 // produce and consume one Task using only one thread
 TEST_F(MDListUt, OneThreadProduceAndConsume)
 {
     auto sumOne = [](const int& n) {return n + 1;};
 
-    auto retNum = list->Produce(0, sumOne, 1);
-    /* EXPECT_EQ(list->TasksCount(), 1); */
-    list->Consume();
+    auto retNum = _list->Produce(2, sumOne, 1);
+    _list->Consume();
 
     EXPECT_EQ(retNum.get(), 2);
+}
+
+// return a vector of futures from the Tasks added to the Queue
+auto getFutures(MDList* list, const int& numOfTasks) {
+
+    auto sumOne = [](const int& n) {return n + 1;};
+    // first value is espacted value and second value is the return
+    std::vector<std::pair<uint32_t, std::future<int>>> futures;
+
+    for (int i = 0; i < numOfTasks; i++) {
+        for (int j = 0; j < kPriorityCount; j++) {
+            futures.emplace_back(std::pair<uint32_t, std::future<int>>
+                    (i + 1, list->Produce(j, sumOne, i)));
+        }
+    }
+    return futures;
+}
+
+// multiple producers and multiple consumers with multiple Tasks
+TEST_F(MDListUt, MultiProducersMultiConsumersMultiTasks)
+{
+    std::atomic<bool> exit;
+    exit.exchange(false);
+    const int numOfTasks = 1000;
+
+    auto consume = [&exit](MDList* list) {while(!exit) {list->Consume();}};
+
+    std::thread t1(consume, this->_list);
+    std::thread t2(consume, this->_list);
+    std::thread t3(consume, this->_list);
+    std::thread t4(consume, this->_list);
+
+    auto futures1 = std::async(getFutures, _list, numOfTasks);
+    auto futures2 = std::async(getFutures, _list, numOfTasks);
+    auto futures3 = std::async(getFutures, _list, numOfTasks);
+
+    auto futuresV1 = futures1.get();
+    auto futuresV2 = futures2.get();
+    auto futuresV3 = futures3.get();
+
+    for (int i = 0; i < numOfTasks; i++) {
+        EXPECT_EQ(futuresV1.at(i).second.get(), futuresV1.at(i).first);
+        EXPECT_EQ(futuresV2.at(i).second.get(), futuresV2.at(i).first);
+        EXPECT_EQ(futuresV3.at(i).second.get(), futuresV3.at(i).first);
+    }
+
+    exit.exchange(true);
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
 }
